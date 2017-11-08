3f2e44be92f8ef7eb09522d8e05c1aa745efb0c7
Ubuntu <vagrant@Base-Box>
2017-11-08 06:16:32 +0000

    Added image column to Products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107232433) do
+ActiveRecord::Schema.define(version: 20171108061530) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20171107232433) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "provinces", force: :cascade do |t|
