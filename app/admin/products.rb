f324f61a6721454b0475163d903eeac3ce021a71
Marco Orcullo <morcullo@academic.rrc.ca>
2017-11-08 06:43:55 +0000

    Added image for product

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Product do
-  permit_params :name, :description, :price, :stock_quantity, :category_id
+  permit_params :name, :description, :price, :stock_quantity, :category_id, :image
 end
