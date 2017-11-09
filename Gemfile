d059f1cd7932010a2b1586ac5d7f2c02f15975d9
Marco Orcullo <morcullo@academic.rrc.ca>
2017-11-09 04:19:30 +0000

    Added Kaminari for pagination

--- Gemfile
+++ Gemfile
@@ -58,3 +58,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'activeadmin', github: 'activeadmin'
 gem 'bootstrap-sass'
+gem 'kaminari'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @products = Product.all
+    @products = Kaminari.paginate_array(@products).page(params[:page]).per(8)
     @categories = Category.all
   end
 end
