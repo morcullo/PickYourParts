67571f045cc20b0285bb9bb7bff0095b193d51b9
Marco Orcullo <morcullo@academic.rrc.ca>
2017-11-10 01:08:22 +0000

    Fixed scaling issue with categories dropdown and updated nav look

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.order(:name)
     @products = Kaminari.paginate_array(@products).page(params[:page]).per(8)
     @categories = Category.all
   end
